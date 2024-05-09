fb2d977fbf8da7534badb506a41548d4ac469782
igor moura <igormoura1180@gmail.com>
2024-05-09 10:05:25 -0300

    exercicio 3

---
+++ Exercicio3.js
@@ -0,0 +1,9 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = parseFloat(prompt("Insira um numero qualquer: "))
+
+if (numero % 2 == 0)
+{console.log ("O numero e par")}
+
+else (console.log ("O numero e impar"))
