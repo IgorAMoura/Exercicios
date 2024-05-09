43c2662f63d87e12b888f1ae643972b103d4144a
igor moura <igormoura1180@gmail.com>
2024-05-09 09:26:49 -0300

    exercicio 2

--- Exercicio2.js
+++ Exercicio2.js
@@ -3,6 +3,28 @@
 // semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
 // 2 representa terça-feira e assim por diante.
 
-let numero ("Insira um numero de 1 a 7 " )
+let numero = prompt("Insira um numero de 1 a 7: " )
 
-if (numero == (1))
+if (numero == "1")
+{console.log ("Segunda-feira")}
+
+else if (numero == "2")
+{console.log ("Terça-feira")}
+
+else if (numero == "3")
+{console.log ("Quarta-feira")}
+
+else if (numero == "4")
+{console.log ("Quinta-feira")}
+
+else if (numero == "5")
+{console.log ("Sexta-feira")}
+
+else if (numero == "6")
+{console.log ("Sabado")}
+
+else if (numero == "7")
+{console.log ("Domingo")}
+
+else {console.log ("Nenhum dia correspondente")
+alert ("Insira um numero valido")}
