9e59b8577c9c1d5929140540245e29e7d9bd6ba6
igor moura <igormoura1180@gmail.com>
2024-05-10 08:17:21 -0300

    exercicio 6

--- Exercicio5.js
+++ Exercicio5.js
@@ -2,3 +2,14 @@
 // Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
 // crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou não.
 
+let primeiro = parseInt(prompt("Insira o primeiro numero: "))
+
+let segundo = parseInt(prompt("Insira o segundo numero: "))
+
+let terceiro = parseInt(prompt("Insira o terceiro numero: "))
+
+if (primeiro < segundo < terceiro)
+{console.log ("esta em ordem crescente")}
+
+else {console.log ("não esta em ordem crescente")}
+
