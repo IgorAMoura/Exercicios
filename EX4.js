c6043cda400413f538178396aef1be211248f47e
igor moura <igormoura1180@gmail.com>
2024-05-14 10:04:09 -0300

    exercicio 4

---
+++ EX4.js
@@ -0,0 +1,8 @@
+// 4. Faça um programa que imprima na tela a tabuada de um número
+
+let numero = parseInt(prompt("insira um numero: "))
+{console.log("tabuada de 1 a 10: ")}
+
+for(multi = 1; multi <= 10; multi++)
+{console.log(numero + "x" + multi + "=" + (numero*multi)) }
+
