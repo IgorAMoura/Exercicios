3abeb9f9acadbc40465e64c8e930d865bf4f4cff
igor moura <igormoura1180@gmail.com>
2024-05-15 09:54:28 -0300

    exercicio 10

---
+++ EX10.js
@@ -0,0 +1,8 @@
+// 10aça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let numero = parseInt(prompt("insira"))
+
+do{numero = parseInt(prompt("digite um numero: "))}
+while(numero !== 0)
+
