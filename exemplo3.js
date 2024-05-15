1dcfe25e58de109f27a962242b20b2054d9fae67
igor moura <igormoura1180@gmail.com>
2024-05-15 10:54:43 -0300

    function

---
+++ exemplo3.js
@@ -0,0 +1,25 @@
+// function
+// function saudacao()
+// {let nome = prompt("insira seu nome ")
+// let hora = new Date().getHours()
+
+// if(hora > 6 && hora < 13)
+// {window.alert("bom dia " + nome)}
+// else if(hora >= 13 && hora < 18)
+// {window.alert("boa tarde " + nome)}
+// else{window.alert("boa noite " + nome)}}
+
+// saudacao()
+
+// function somar(num1, num2, num3)
+// {let soma = num1 + num2 + num3
+// return soma}
+// let resultado = somar(5, 6, 9)
+
+// objeto
+let professor = {}
+professor.nome = prompt("digite o nome ")
+professor.sobrenome = prompt("digite o sobrenome ")
+professor.idade = parseInt(prompt("digite a idade "))
+
+console.log(professor)
